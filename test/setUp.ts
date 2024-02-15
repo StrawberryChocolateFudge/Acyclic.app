4ffd6fc05aa34612ebe2ec28f6cc9277abf93b59
StrawberryChocolateFudge <hopeter@protonmail.com>
2024-02-15 21:25:32 +0100

    updated setup to inculde token registry

--- test/setUp.ts
+++ test/setUp.ts
@@ -1,36 +1,50 @@
-import { expect } from "chai";
-import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
-import { Contract } from "ethers";
 import { ethers } from "hardhat";
 import { parseEther } from "ethers/lib/utils";
 
 export async function setUpPLRM(): Promise<any> {
-    const [owner, alice, bob] = await ethers.getSigners();
+  const [owner, alice, bob] = await ethers.getSigners();
+  // Deploy example ERC20 contracts
+  const BTCFactory = await ethers.getContractFactory("TestERC20");
+  const BTCDeploy = await BTCFactory.deploy("BTC", "BTC", parseEther("100"));
+  const BTC = await BTCDeploy.deployed();
 
-    const BTCFactory = await ethers.getContractFactory("TestERC20");
-    const BTCDeploy = await BTCFactory.deploy("BTC", "BTC", parseEther("100"));
-    const BTC = await BTCDeploy.deployed();
+  const USDFactory = await ethers.getContractFactory("TestERC20");
+  const USDDeploy = await USDFactory.deploy(
+    "USD",
+    "USD",
+    parseEther("100000000"),
+  );
+  const USD = await USDDeploy.deployed();
 
-    const USDFactory = await ethers.getContractFactory("TestERC20");
-    const USDDeploy = await USDFactory.deploy("USD", "USD", parseEther("100000000"));
-    const USD = await USDDeploy.deployed();
+  const EURFactory = await ethers.getContractFactory("TestERC20");
 
-    const EURFactory = await ethers.getContractFactory("TestERC20");
+  const EURDeploy = await EURFactory.deploy(
+    "EUR",
+    "EUR",
+    parseEther("100000000"),
+  );
+  const EUR = await EURDeploy.deployed();
 
-    const EURDeploy = await EURFactory.deploy("EUR", "EUR", parseEther("100000000"));
-    const EUR = await EURDeploy.deployed();
+  const ETHFactory = await ethers.getContractFactory("TestERC20");
+  const ETHDeploy = await ETHFactory.deploy("ETH", "ETH", parseEther("100"));
+  const ETH = await ETHDeploy.deployed();
 
+  // Deploy the RequestedTokens contract first to request tokens to be used by the registry
+  const RequestedTokensFactory = await ethers.getContractFactory(
+    "RequestedTokens",
+  );
+  const requestedTokens = await RequestedTokensFactory.deploy();
+  await requestedTokens.deployed();
 
-    const ETHFactory = await ethers.getContractFactory("TestERC20");
-    const ETHDeploy = await ETHFactory.deploy("ETH", "ETH", parseEther("100"));
-    const ETH = await ETHDeploy.deployed();
+  // Deploy a couple of ERC20 tokens with different names and symbols for testing!
 
-    // Deploy a couple of ERC20 tokens with different names and symbols for testing!
+  // Then deploy the PolymerRegistry!
+  const registryFactory = await ethers.getContractFactory("PolymerRegistry");
+  const registryDeploy = await registryFactory.deploy(
+    500,
+    requestedTokens.address,
+  ); // 500 for a 0.5 percent flash loan fee and 0.25 % deposit fee on both tokens
+  const registry = await registryDeploy.deployed();
 
-    // Then deploy the PolymerRegistry!
-    const registryFactory = await ethers.getContractFactory("PolymerRegistry");
-    const registryDeploy = await registryFactory.deploy(500); // 500 for a 0.5 percent flash loan fee
-    const registry = await registryDeploy.deployed();
-
-    return { owner, BTC, USD, EUR, ETH, alice, bob, registry }
+  return { owner, BTC, USD, EUR, ETH, alice, bob, registry, requestedTokens };
 }
