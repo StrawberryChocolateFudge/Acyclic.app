8a02d3c4a9a269c951cec938c3f6d8138a7fcb52
StrawberryChocolateFudge <hopeter@protonmail.com>
2024-02-16 19:51:45 +0100

    added more comments

--- contracts/clone/FactoryContractVerifier.sol
+++ contracts/clone/FactoryContractVerifier.sol
@@ -3,11 +3,12 @@ pragma solidity ^0.8.12;
 import "../PolymerRegistry.sol";
 
 // Verify the bytecode of the PolymerRegistry when calling initialize on the Polymer contract
-// This library linked to the Polymer contract. 
+// This library linked to the deployed Polymer contract template and secures the initializer function to make sure only a registry contract can call it.
+// This contract stores the keccak256 hash of the PolymerRegistry runtimeCode
+// The runtimeCode is compared to the extcodehash from the contract address we want to validate
 
-//This contract stores the keccak256 hash of the PolymerRegistry runtimeCode
-//The runtimeCode is compared to the extcodehash from the contract address we want to validate
-
+// The library is accessed with "using ... for FactoryContractVerifierstate"
+// The purpose of the counter is for the struct is just to hold a variable, since we can't initialize an empty struct with a method.
 struct FactoryContractVerifierState {
     uint256 count;
 }
