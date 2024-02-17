d5a274d2964fd758978bee048b03560fc4be8eb1
StrawberryChocolateFudge <hopeter@protonmail.com>
2024-02-17 05:31:00 +0100

    feat: refactored deploy, fee, structs

--- contracts/PolymerRegistry.sol
+++ contracts/PolymerRegistry.sol
@@ -34,35 +34,44 @@ contract PolymerRegistry is Ownable, CloneFactory {
     error OnlyAcceptedToken();
     error InvalidRate();
     error OnlyOwner();
+    error NotEnoughDeploymentFee();
 
     string private prefix = "PLMR"; // The prefix of the deployed token contracts
     address private feeReciever; // The address of the flash loan reciever
     uint256 private depositFeeDivider; // The fee for the deposit
     uint256 public lastIndex; // The index of the last PLMR contract deployed!
 
-    IRequestedTokens private requestedTokens;
+    IRequestedTokens private requestedTokens; // The token contract for requested tokens
 
     mapping(address => bool) public isPolymerAddress; // A helper mapping to check if an address is registered here
 
     // A struct used for easily accessing all saved Polymers on the Front end!
     struct PLMR {
-        address polymerAddress;
-        address token1Addr;
-        string token1Ticker;
-        uint256 token1Rate;
-        uint8 token1DecimalShift;
-        address token2Addr;
-        string token2Ticker;
-        uint256 token2Rate;
-        uint8 token2DecimalShift;
         string plmrName;
         string plmrSymbol;
+        address polymerAddress;
+        address token1Addr;
+        string token1Symbol;
+        uint256 token1Rate;
+        uint8 token1DecimalShift;
+        bool token1IsPlmr;
+        address token2Addr;
+        string token2Symbol;
+        uint256 token2Rate;
+        uint8 token2DecimalShift;
+        bool token2IsPlmr;
     }
 
     PLMR[] private allpolymers; // All the plmrs are stored in an array
 
     // Emit a NewPLMR event. The front end will listen to this event to navigate to the next page.
     event NewPLMR(PLMR);
+
+    // A mapping to store how many times a token address was deployed with a PLMR token.
+    // This is used for the SPAM protection, creating more PLMR token contracts with a token, that is already wrapped will get progressively more expensive! This is used to disincentivise spam attacks.
+    mapping(address => uint256) private deploymentCounter;
+    // The base fee for a new PLMR contract deployment is 0.05 ether. The base fee is multiplied with the deploymentCounter. The first deployment is always free.
+    uint256 constant PLMRDeploymentBaseFee = 0.05 ether;
 
     /**
    @dev The costructor is invoked when deploying the contract
@@ -117,6 +126,8 @@ contract PolymerRegistry is Ownable, CloneFactory {
     @param token2Addr the address of the token2
     @param token2Rate the rate of the token2
     @param token2DecimalShift The amount we shift the decimals of the token2 when calculating deposits and redemptions
+    The msg.value must match the totalDeploymentFee, this fee exists to mitigate a spam attack on a external function that can be called by anyone. 
+    The first tokens are free to deploy, then the price is 0.05 ETH and doubles with each new deployment. it is counted per token address.
      */
 
     function createNewPLMR(
@@ -126,7 +137,7 @@ contract PolymerRegistry is Ownable, CloneFactory {
         address token2Addr,
         uint256 token2Rate,
         uint8 token2DecimalShift
-    ) external {
+    ) external payable {
         if (!(token1DecimalShift <= ERC20(token1Addr).decimals()))
             revert InvalidDecimals();
         if (!(token2DecimalShift <= ERC20(token2Addr).decimals()))
@@ -135,35 +146,18 @@ contract PolymerRegistry is Ownable, CloneFactory {
         if (token1Rate == 0) revert InvalidRate();
         if (token2Rate == 0) revert InvalidRate();
 
-        // If the address is not a PLMR address
-        if (!isPolymerAddress[token1Addr]) {
-            // Only allow creating token pairs that were reviewed
-            if (requestedTokens.getStatus(token1Addr) != TokenStatus.ACCEPTED)
-                revert OnlyAcceptedToken();
-        }
-        // If the address 2 is not a PLMR address
-        if (!isPolymerAddress[token2Addr]) {
-            // Only allow creating token pairs that were reviewed
-            if (requestedTokens.getStatus(token2Addr) != TokenStatus.ACCEPTED)
-                revert OnlyAcceptedToken();
-        }
-        //Increment the index for the name of the token
-        lastIndex++;
-        //Stringify the new index so I can concat it into a name and symbol
-        string memory index = Strings.toString(lastIndex);
-        //plmrName stores the token anme and symbol in an array
-        string[2] memory plmrName;
-        // Name should be something like PLMR4-USDT/ETH
-        plmrName[0] = string.concat(
-            prefix,
-            index,
-            "-",
-            ERC20(token1Addr).symbol(),
-            "/",
-            ERC20(token2Addr).symbol()
+        _validateTokenAddresses(token1Addr, token2Addr);
+
+        uint256 totalDeploymentFee = getDeploymentFee(token1Addr).add(
+            getDeploymentFee(token2Addr)
         );
-        // Ticker should be like PLMR4, prefix+index
-        plmrName[1] = string.concat(prefix, index);
+
+        if (totalDeploymentFee != msg.value) revert NotEnoughDeploymentFee();
+        _forwardDeploymentFee();
+        _incrementDeploymentCounter(token1Addr);
+        _incrementDeploymentCounter(token2Addr);
+
+        string[2] memory plmrName = _concatTokenName(token1Addr, token2Addr);
 
         // Create a new Polymer contract by cloning the library
 
@@ -176,20 +170,76 @@ contract PolymerRegistry is Ownable, CloneFactory {
 
         // Save the address of the deployed contract
         saveNewPLMRDetails(
-            PLMR(
-                address(initializedPolymer),
-                token1Addr,
-                ERC20(token1Addr).name(),
-                token1Rate,
-                token1DecimalShift,
-                token2Addr,
-                ERC20(token2Addr).name(),
-                token2Rate,
-                token2DecimalShift,
-                plmrName[0],
-                plmrName[1]
-            )
+            PLMR({
+                plmrName: plmrName[0],
+                plmrSymbol: plmrName[1],
+                polymerAddress: address(initializedPolymer),
+                token1Addr: token1Addr,
+                token1Symbol: ERC20(token1Addr).symbol(),
+                token1Rate: token1Rate,
+                token1DecimalShift: token1DecimalShift,
+                token1IsPlmr: isPolymerAddress[token1Addr],
+                token2Addr: token2Addr,
+                token2Symbol: ERC20(token2Addr).symbol(),
+                token2Rate: token2Rate,
+                token2DecimalShift: token2DecimalShift,
+                token2IsPlmr: isPolymerAddress[token2Addr]
+            })
         );
+    }
+
+    // Validate the token addresses are accepted to be used
+    function _validateTokenAddresses(
+        address token1Addr,
+        address token2Addr
+    ) internal view {
+        // If the address is not a PLMR address
+        if (!isPolymerAddress[token1Addr]) {
+            // Only allow creating token pairs that were reviewed
+            if (requestedTokens.getStatus(token1Addr) != TokenStatus.ACCEPTED)
+                revert OnlyAcceptedToken();
+        }
+        // If the address 2 is not a PLMR address
+        if (!isPolymerAddress[token2Addr]) {
+            // Only allow creating token pairs that were reviewed
+            if (requestedTokens.getStatus(token2Addr) != TokenStatus.ACCEPTED)
+                revert OnlyAcceptedToken();
+        }
+    }
+
+    /**
+     * @dev Forwards the funds to the feeReciever
+     */
+    function _forwardDeploymentFee() internal {
+        payable(feeReciever).transfer(msg.value);
+    }
+
+    // This function modifies state and increments the deployment counter!
+    function _incrementDeploymentCounter(address ofToken) internal {
+        deploymentCounter[ofToken] += 1;
+    }
+
+    //Concat the Token name
+    function _concatTokenName(
+        address token1Addr,
+        address token2Addr
+    ) internal returns (string[2] memory plmrName) {
+        //Increment the index for the name of the token
+        lastIndex++;
+        //Stringify the new index so I can concat it into a name and symbol
+        string memory index = Strings.toString(lastIndex);
+        //plmrName stores the token name and symbol in an array
+        // Name should be something like PLMR4-USDT/ETH and will be the .name() of the ERC-20 token
+        plmrName[0] = string.concat(
+            prefix,
+            index,
+            "-",
+            ERC20(token1Addr).symbol(),
+            "/",
+            ERC20(token2Addr).symbol()
+        );
+        // Symbol should be like PLMR4, prefix+index, it will be the .symbol() of the ERC-20 token
+        plmrName[1] = string.concat(prefix, index);
     }
 
     // Call clone and initualize the new Polymer contract
@@ -239,5 +289,11 @@ contract PolymerRegistry is Ownable, CloneFactory {
         uint256 index
     ) external view returns (PLMR memory) {
         return allpolymers[index];
+    }
+
+    // Get the deployment fee of a token by multiplying it with the counter
+    // This is public so it's called internally but also by the front end to calculate the fees needed
+    function getDeploymentFee(address ofToken) public view returns (uint256) {
+        return deploymentCounter[ofToken].mul(PLMRDeploymentBaseFee);
     }
 }
