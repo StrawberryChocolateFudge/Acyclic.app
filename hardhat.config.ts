c8a72a5afe7bce7e73cf78f10a178d71ca47f7dd
StrawberryChocolateFudge <hopeter@protonmail.com>
2024-02-20 00:16:43 +0100

    added sepolina testnet url for deployment

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -30,8 +30,8 @@ const config: HardhatUserConfig = {
     ]
   },
   networks: {
-    ropsten: {
-      url: process.env.ROPSTEN_URL || "",
+    sepolina: {
+      url: process.env.ARBITRUM_SEPOLINA_URL || "",
       accounts:
         process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
