3a5485950c1702d54e776c918a824a75668f4449
StrawberryChocolateFudge <hopeter@protonmail.com>
2024-02-20 03:29:36 +0100

    added copy to abi

--- copyToDist.sh
+++ copyToDist.sh
@@ -1,1 +1,9 @@
 cp -r frontend/imgs/ dist/imgs
+
+cp artifacts/contracts/AGPH.sol/AGPH.json dist/AGPH.json
+cp artifacts/contracts/RequestedTokens.sol/RequestedTokens.json dist/RequestedTokens.json
+cp artifacts/contracts/GraphStore.sol/GraphStore.json dist/GraphStore.json
+
+cp artifacts/@openzeppelin/contracts/token/ERC20/ERC20.sol/ERC20.json dist/ERC20.json
+
+cp artifacts/contracts/MOCK/TestERC20.sol/TestERC20.json dist/TestERC20.json
