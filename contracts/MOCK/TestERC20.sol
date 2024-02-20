efc38c8aff3fdad16089806dcc0958603a7896f7
StrawberryChocolateFudge <hopeter@protonmail.com>
2024-02-20 03:30:04 +0100

    Anyone can mint testnet tokens!

--- contracts/MOCK/TestERC20.sol
+++ contracts/MOCK/TestERC20.sol
@@ -9,32 +9,8 @@ import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 import "@openzeppelin/contracts/utils/Context.sol";
 
 // DO NOT DEPLOY ON MAINNET, CREATED FOR TESTING PURPOSES ONLY!!
+// ANYONE CAN MINT THESE TOKENS!
 
-/**
- * @dev Implementation of the {IERC20} interface.
- *
- * This implementation is agnostic to the way tokens are created. This means
- * that a supply mechanism has to be added in a derived contract using {_mint}.
- * For a generic mechanism see {ERC20PresetMinterPauser}.
- *
- * TIP: For a detailed writeup see our guide
- * https://forum.openzeppelin.com/t/how-to-implement-erc20-supply-mechanisms/226[How
- * to implement supply mechanisms].
- *
- * We have followed general OpenZeppelin Contracts guidelines: functions revert
- * instead returning `false` on failure. This behavior is nonetheless
- * conventional and does not conflict with the expectations of ERC20
- * applications.
- *
- * Additionally, an {Approval} event is emitted on calls to {transferFrom}.
- * This allows applications to reconstruct the allowance for all accounts just
- * by listening to said events. Other implementations of the EIP may not emit
- * these events, as it isn't required by the specification.
- *
- * Finally, the non-standard {decreaseAllowance} and {increaseAllowance}
- * functions have been added to mitigate the well-known issues around setting
- * allowances. See {IERC20-approve}.
- */
 contract TestERC20 is Context, IERC20, IERC20Metadata {
     mapping(address => uint256) private _balances;
 
@@ -62,6 +38,11 @@ contract TestERC20 is Context, IERC20, IERC20Metadata {
         _name = name_;
         _symbol = symbol_;
         _mint(_msgSender(), initialSupply);
+    }
+
+    // THIS IS A TESTNET FUNCTION DO NOT DEPLOY THIS ON MAINNET!!
+    function MintTestnetTokens(uint256 amount) external {
+        _mint(_msgSender(), amount);
     }
 
     /**
